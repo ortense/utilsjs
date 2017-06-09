80489e789e74538a6190966f1fef219a03ae297f
Astesio <astesiojose@gmail.com>
2017-06-08 23:50:47 -0300

    create a function for validate if an email is valid or no

--- src/string/string-util.js
+++ src/string/string-util.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const isValidEmail = (email) => {
+
+    const regexp = /^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$/;
+    return regexp.test(email);
+};
+
+module.exports = Object.assign({}, {isValidEmail});
