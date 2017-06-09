9b0510ab78c68738d189adc0b4abf7b4efdfffe7
Astesio <astesiojose@gmail.com>
2017-06-09 20:28:15 -0300

    change methods

--- src/string/string-util.js
+++ src/string/string-util.js
@@ -1,9 +1,12 @@
 'use strict';
 
-const isValidEmail = (email) => {
-
-    const regexp = /^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$/;
-    return regexp.test(email);
+const string = {
+    
+    isValidEmail: (email) => {
+        
+        const regexp = /^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$/;
+        return regexp.test(email);
+    }
 };
 
-module.exports = Object.assign({}, {isValidEmail});
+module.exports = Object.assign({}, {string});
