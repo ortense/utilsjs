3c4e319ea220231206add0f53fedb2df8146f19c
Astesio <astesiojose@gmail.com>
2017-06-09 23:55:56 -0300

    fix(remove all test don't using): I remove all tests don't using until now

--- src/boolean/boolean-util.spec.js
+++ src/boolean/boolean-util.spec.js
@@ -1,14 +1,1 @@
 'use strict';
-
-const expect = require('chai').expect;
-
-describe('Boolean', () => {
-    
-    describe('method', () => {
-        
-        it('should work', () => {
-            
-            expect(true).to.be.true;
-        })
-    });
-});
